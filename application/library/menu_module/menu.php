6330824c06acd8a3d30359e8854b44586d676637
marcelo1091 <kw-53@wp.pl>
2019-12-18 11:56:05 +0100

    add sidebar

--- application/library/menu_module/menu.php
+++ application/library/menu_module/menu.php
@@ -1,17 +1,26 @@
-<?php
-
-/**
- * ModuleName: Menu główne
- * ModuleDesc: Menu głowne serwisu SuperCMS
- */
-
-echo '<div class="content">       
-        <h1><a href="/">Design klasy biznes</a></h1>        
-        <ul>
-            <li><a href="home/index">Home</a></li>
-            <li><a href="administrator/index">ADMIN</a></li>'.
-            model_load('homemodel', 'addLogoutBtn', '').'
-        </ul>
-    </div>';
-
-?>
+<div id="navbar" class="navbar-collapse collapse">
+    <ul class="nav navbar-nav">
+        <li class="active"><a href="dashboard.php">Dashboard</a></li>
+        
+        <li class="dropdown">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Adressen <span class="caret"></span></a>
+        'aaa'
+        </li>
+        
+        <li><a href="wplaty_adresy.php">'.Inkomsten.'</a></li>
+        <li><a href="wplaty.php">Overige inkomsten</a></li>
+        <li><a href="wyplaty_adresy.php">'.Uitgaven.'</a></li>
+        <li><a href="wyplaty.php">Overige uitgaven</a></li>
+        <li><a href="reperatie.php">Reparatie</a></li>
+        <li><a href="takken.php">Takken</a></li>';
+        <li><a href="cashbook.php">'.Cashbook.'</a></li>
+        <li><a href="kruisposten.php">'.Kruisposten.'</a></li>
+        <li><a href="import_940.php">'.Importeren.'</a></li>
+        <li><a href="agenda.php">'.Agenda.'</a></li>
+        <li><a href="users.php">'.Gebruikers.'</a></li> 
+        <li><a href="lista_rejestracja.php">'.Registratie.'</a></li>		
+        <li><a href="ustawienia.php">'.Instellingen.'</a></li>';
+        
+    </ul>
+    
+</div><!--/.nav-collapse -->
